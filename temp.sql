641f813122d5002cf0036ec71903711c315ff439
mbcladwell <mbcladwell@stihie.net>
2019-04-09 16:17:12 -0400

    tues even

--- temp.sql
+++ temp.sql
@@ -165,6 +165,84 @@ plate_layout.well_type_id=1
 plate_layout.well_by_col= well.by_col
 
 
+select sample.id from well, well_sample, sample WHERE well_sample.well_id=well.ID AND well_sample.sample_id=sample.ID AND  well.plate_id=403;
+
+select * from well WHERE   well.plate_id=403 LIMIT 5;
 
 
+---from get wells
+ "SELECT plate.plate_sys_name AS \"PlateID\", well_numbers.well_name AS \"Well\", well.by_col AS \"Well_NUM\", sample.sample_sys_name AS \"Sample\", sample.accs_id as \"Accession\" FROM  plate, sample, well_sample, well JOIN well_numbers ON ( well.by_col= well_numbers.by_col)  WHERE plate.id = well.plate_id AND well_sample.well_id=well.id AND well_sample.sample_id=sample.id AND well.plate_id = (SELECT plate.id FROM plate WHERE plate.plate_sys_name = ?) AND  well_numbers.plate_format = (SELECT plate_format_id  FROM plate_set WHERE plate_set.ID =  (SELECT plate_set_id FROM plate_plate_set WHERE plate_id = plate.ID LIMIT 1) ) ORDER BY well.by_col DESC;");
 
+
+ SELECT plate.plate_sys_name AS "PlateID", well_numbers.well_name AS "Well", well.by_col AS "Well_NUM", sample.sample_sys_name AS "Sample", sample.accs_id as "Accession" FROM  plate, sample, well_sample, well JOIN well_numbers ON ( well.by_col= well_numbers.by_col)  WHERE plate.id = well.plate_id AND well_sample.well_id=well.id AND well_sample.sample_id=sample.id AND well.plate_id = (SELECT plate.id FROM plate WHERE plate.plate_sys_name = 'PLT-400') AND  well_numbers.plate_format = (SELECT plate_format_id  FROM plate_set WHERE plate_set.ID =  (SELECT plate_set_id FROM plate_plate_set WHERE plate_id = plate.ID LIMIT 1) ) ORDER BY well.by_col DESC;
+
+
+SELECT * FROM sample LIMIT 5;
+SELECT * FROM plate_set LIMIT 5;
+SELECT * FROM plate_plate_set LIMIT 5;
+SELECT * FROM well LIMIT 5;
+SELECT * FROM well_sample LIMIT 5;
+SELECT * FROM plate LIMIT 5;
+
+SELECT * FROM plate, well, well_sample, sample WHERE plate.id=400 AND well.plate_id=plate.ID AND well_sample.sample_id=sample.ID AND well_sample.well_id=well.ID;
+
+SELECT * FROM well WHERE well.plate_id = 403;  --get 96 rows
+
+268997 |      5 |      403
+ 268996 |      4 |      403
+ 268995 |      3 |      403
+ 268994 |      2 |      403
+ 268993 |      1 |      403
+
+SELECT * FROM well_sample WHERE well_sample.well_id = 268993 LIMIT 5;
+
+--I am NOT updating well_sample
+
+
+DROP FUNCTION rearray_transfer_samples(integer, INTEGER, integer);
+CREATE OR REPLACE FUNCTION rearray_transfer_samples(source_plate_set_id INTEGER, dest_plate_set_id INTEGER, hit_list_id integer)
+ RETURNS void AS
+$BODY$
+DECLARE
+   i INTEGER;
+all_hit_sample_ids INTEGER[];
+dest_wells INTEGER[];
+num_hits INTEGER;
+rp_id INTEGER;
+
+BEGIN
+--select get in plate, well order, not necessarily sample order 
+SELECT ARRAY (SELECT  sample.id FROM plate_set, plate_plate_set, plate, well, well_sample, sample WHERE plate_plate_set.plate_set_id=plate_set.ID AND plate_plate_set.plate_id=plate.id AND well.plate_id=plate.ID AND well_sample.well_id=well.ID AND well_sample.sample_id=sample.ID and plate_set.id=source_plate_set_id AND sample.ID  IN  (SELECT hit_sample.sample_id FROM hit_sample WHERE hit_sample.hitlist_id = hit_list_id) ORDER BY plate.ID, well.ID) INTO all_hit_sample_ids;
+
+num_hits := array_length(all_hit_sample_ids, 1);
+raise NOTice 'num_hits: (%)', num_hits;
+SELECT ARRAY (SELECT well.ID FROM plate_set, plate_plate_set, plate, well, plate_layout WHERE plate_plate_set.plate_set_id=plate_set.ID AND plate_plate_set.plate_id=plate.id AND well.plate_id=plate.ID AND plate_set.plate_layout_name_id=plate_layout.plate_layout_name_id AND plate_layout.well_by_col= well.by_col AND plate_set.id=22 AND plate_layout.well_type_id=1 ORDER BY well.ID) INTO dest_wells;
+
+
+  for i IN 1..num_hits
+  loop
+  INSERT INTO well_sample (well_id, sample_id) VALUES ( dest_wells[i], all_hit_sample_ids[i]);   
+raise NOTice 'dest_well: (%)', dest_wells[i];
+
+END LOOP;
+
+INSERT INTO rearray_pairs (src, dest) VALUES (source_plate_set_id, dest_plate_set_id)  returning id INTO rp_id;
+
+CREATE TEMP TABLE temp1 (plate_sys_name VARCHAR(10), by_col INTEGER, sample_id INTEGER);
+
+INSERT INTO temp1 SELECT  plate.plate_sys_name, well.by_col, sample.ID AS "sample_id"  FROM plate_set, plate_plate_set, plate, well, well_sample, sample  WHERE plate_plate_set.plate_set_id=plate_set.ID AND plate_plate_set.plate_id=plate.id AND well.plate_id=plate.ID AND well_sample.well_id=well.ID AND well_sample.sample_id=sample.ID and plate_set.id=source_plate_set_id  AND sample.ID IN  (SELECT hit_sample.sample_id FROM hit_sample WHERE hit_sample.hitlist_id = hit_list_id ORDER BY sample.ID);
+
+CREATE TEMP TABLE temp2 (plate_sys_name VARCHAR(10), by_col INTEGER, sample_id INTEGER);
+
+INSERT INTO temp2 SELECT  plate.plate_sys_name, well.by_col, sample.ID AS "sample_id" FROM plate_set, plate_plate_set, plate, well, well_sample, sample  WHERE plate_plate_set.plate_set_id=plate_set.ID AND plate_plate_set.plate_id=plate.id AND well.plate_id=plate.ID AND well_sample.well_id=well.ID AND well_sample.sample_id=sample.ID and plate_set.id=dest_plate_set_id  ORDER BY sample.ID;
+
+INSERT INTO worklists ( rearray_pairs_id, sample_id, source_plate, source_well, dest_plate, dest_well) SELECT rp_id, temp1.sample_id, temp1.plate_sys_name, temp1.by_col, temp2.plate_sys_name, temp2.by_col FROM temp1, temp2 WHERE temp1.sample_id = temp2.sample_id;
+
+DROP TABLE temp1, temp2;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+
+SELECT rearray_transfer_samples(21,22,21);

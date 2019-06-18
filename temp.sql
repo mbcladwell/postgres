3d806d997f9f36a789c91dfdd9febc70e969dbda
mbcladwell <mbcladwell@stihie.net>
2019-06-18 16:34:41 -0400

    tues even

--- temp.sql
+++ temp.sql
@@ -48,14 +48,13 @@ SELECT * FROM lnsession;
 SELECT plate_set.plate_set_sys_name , plate.plate_sys_name as "Plate", assay_result.well, sample.sample_sys_name, assay_result.response, assay_result.bkgrnd_sub, assay_result.norm, assay_result.norm_pos  FROM assay_result, assay_run, plate_plate_set, plate_set, plate, well_sample, sample, well LEFT OUTER JOIN well_numbers ON (well.by_col = well_numbers.by_col) WHERE assay_run.plate_set_id=plate_plate_set.plate_set_id and assay_result.assay_run_id=assay_run.id AND plate_plate_set.plate_order=assay_result.plate_order AND assay_result.well=well.by_col   and plate_plate_set.plate_set_id=plate_set.id AND plate_plate_set.plate_id=plate.ID and plate.id=well.plate_id AND well.id=well_sample.well_id AND sample.ID = well_sample.sample_id AND plate_set.plate_format_id=well_numbers.plate_format AND assay_run.id IN (3 ,2 ,1) ORDER BY plate.plate_sys_name, assay_result.well desc;   LIMIT 5;
 
 
-
 SELECT * FROM well_numbers LIMIT 5;
-SELECT * FROM plate_set LIMIT 5;
+SELECT * FROM plate_set WHERE plate_set.id=5;  LIMIT 5;
 SELECT * FROM plate LIMIT 5;
 SELECT * FROM well LIMIT 5;
 SELECT * FROM well_sample LIMIT 5;
 SELECT * FROM sample LIMIT 5;
-SELECT * FROM assay_run LIMIT 5;
-SELECT * FROM assay_result LIMIT 5;
+SELECT * FROM assay_run WHERE assay_run.ID = 3;  LIMIT 5;
+SELECT * FROM assay_result WHERE assay_result.assay_run_ID = 3;   5;
 
 SELECT *  FROM assay_result, assay_run WHERE assay_result.assay_run_id=assay_run.ID and assay_run.ID IN (3) LIMIT 5;
